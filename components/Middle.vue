df53938f566751c5b114fac653c7fd61300bc6de
Anne Silva <fabiannecs@hotmail.com>
2021-02-16 01:45:47 -0300

    First commit

---
+++ components/Middle.vue
@@ -0,0 +1,20 @@
+<template>
+  <main class="middle">
+    <section class="middle-grid">
+      <h2>Encontre pessoas para adotar um pet</h2>
+      <p class="p-header">
+        Nós sabemos os desafios de se ter um ou mais pets, <br />
+        por isso criamos um espaço especial para você divulgar a adoção
+        <br />
+        dos seus pets caso queira doá-los.
+      </p>
+      <a class="btn btn-middle" href="#">Encontre uma ONG</a>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container-fluid">
+    <div>
+      <Header />
+      <Middle />
+      <Footer />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+</style>
