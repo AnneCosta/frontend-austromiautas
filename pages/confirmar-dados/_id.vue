3775e77dc287e4d57e147795ed36daf2626484f0
Gabriel Silva <gabrieljsilva2301@gmail.com>
2021-04-03 14:42:37 -0300

    ajustes

--- pages/confirmar-dados/_id.vue
+++ pages/confirmar-dados/_id.vue
@@ -4,14 +4,14 @@
       <nav
         class="flex flex-wrap justify-center pb-4 md:justify-between md:pb-0 items-center bg-primary pt-4"
       >
-        <img src="../static/image/logo_no_bg.png" alt="" width="150" />
+        <img src="/image/logo_no_bg.png" alt="" width="150" />
         <nuxt-link to="/entrar">
           <a-button class="md:mr-5 mt-4 md:mt-0 text-white" size="lg">
             Entrar
           </a-button>
         </nuxt-link>
       </nav>
-      <img src="../static/image/wave.png" alt="" width="100%" />
+      <img src="/image/wave.png" alt="" width="100%" />
     </header>
     <main class="container m-auto">
       <h1 class="text-2xl text-center font-bold mt-4 mx-4 md:mx-0">
@@ -20,21 +20,27 @@
       <section class="mx-4 md:mx-auto md:w-1/2">
         <form>
           <section>
-            <a-input label="Nome completo" />
+            <a-input v-model="adopter.name" label="Nome completo" />
           </section>
           <section>
-            <a-input label="CPF" />
+            <a-input v-model="adopter.cpf" label="CPF" />
           </section>
           <section class="my-8">
             <a-input
               id="birth"
+              v-model="adopter.birth"
               label="Data de nascimento"
               type="date"
               elevate-label
             />
           </section>
 
-          <a-button class="mb-4 md:mb-0 w-full my-4" fluid size="md">
+          <a-button
+            class="mb-4 md:mb-0 w-full my-4"
+            fluid
+            size="md"
+            @click="handleAdopt"
+          >
             Confirmar adoção
           </a-button>
         </form>
@@ -42,3 +48,22 @@
     </main>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      adopter: {
+        name: '',
+        cpf: '',
+        birth: null,
+      },
+    }
+  },
+  methods: {
+    handleAdopt() {
+      //
+    },
+  },
+}
+</script>
